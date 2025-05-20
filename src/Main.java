efbd3890489f74307fea4c51821932aefb43cee0
dougs-lima <ytdouglima@gmail.com>
2025-05-20 20:08:15 -0300

    Initial commit java

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.net.SocketOption;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello, Mundo!");
+    }
+}
+class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}
+class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Douglas!\nTudo bem?");
+    }
+}
+class Estudos {
+    public static void main(String[] args) {
+
+    }
+}
+class Soma {
+    public static void main(String[] args) {
+        System.out.println(10+5);
+    }
+}
+class Subtracao {
+    public static void main(String[] args) {
+        System.out.println(10-5);
+    }
+}
