2152469ce02ed1b85822643334f88ebc075b502a
dougs-lima <ytdouglima@gmail.com>
2025-05-21 09:46:41 -0300

    média entre dois números

---
+++ src/Casting.java
@@ -0,0 +1,8 @@
+public class Casting {
+    public static void main(String[] args) {
+        double numero = 12.4;
+        int castingNumero = (int) numero;
+
+        System.out.println(castingNumero);
+    }
+}
---
+++ src/MediaDeDuasNotas.java
@@ -0,0 +1,10 @@
+public class MediaDeDuasNotas {
+    public static void main(String[] args) {
+        double nota1 = 7.5;
+        double nota2 = 9.5;
+
+        double mediaNotas = (nota1 + nota2) / 2;
+
+        System.out.println("A média das notas foi: " + mediaNotas);
+    }
+}
