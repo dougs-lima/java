a154460c2f684e97a6ee81b3160145e0837cedbf
dougs-lima <ytdouglima@gmail.com>
2025-05-30 00:33:18 -0300

    Ajuste array + exercícios

--- src/Array3.java
+++ src/Array3.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class Array2 {
+public class Array3 {
     public static void main(String[] args){
+        int[] vetA = new int[15];
+        int[] vetB = new int[15];
         Scanner scan = new Scanner(System.in);
-        int[] vetA = new int[5];
-        int[] vetB = new int[vetA.length];
 
         for (int i = 0; i < vetA.length; i++) {
-            System.out.println("Insira o valor de A da posição " + i);
+            System.out.println("Digite valor da porsição " + i);
             vetA[i] = scan.nextInt();
 
-            vetB[i] = vetA[i] * 2;
+            vetB[i] = vetA[i] * vetA[i];
         }
         System.out.print("Vetor A: ");
         for (int i = 0; i < vetA.length; i++) {
@@ -19,6 +19,7 @@ public class Array2 {
         System.out.println();
 
         System.out.print("Vetor B: ");
+
         for (int i = 0; i < vetA.length; i++) {
             System.out.print(vetB[i] + " ");
         }
