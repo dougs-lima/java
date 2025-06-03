f12da18747d7fa3c19814b6eb4c8331fbf9fef66
dougs-lima <ytdouglima@gmail.com>
2025-06-03 09:08:23 -0300

    Exercicio modulo aluno

---
+++ src/metodos3/Aluno.java
@@ -0,0 +1,24 @@
+package metodos3;
+
+public class Aluno {
+    String nome;
+    String matricula;
+    String nomeCurso;
+    String[] nomeDisciplinas = new String[3];
+    double[][] notasDisciplinas = new double[3][4];
+
+     void mostrarInfo(){
+         System.out.println("Nome: " + nome);
+         System.out.println("Matrícula: " + matricula);
+         System.out.println("Nome do curso " + nomeCurso);
+
+         for (int i = 0; i < notasDisciplinas.length; i++) {
+             System.out.println("Notas da disciplina " + nomeDisciplinas);
+             for (int j = 0; j < notasDisciplinas[i].length; j++) {
+                 System.out.println(notasDisciplinas[i][j]);
+             }
+             System.out.println();
+             
+         }
+     }
+}
