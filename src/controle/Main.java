e24419b1dde24a17981743c0c44d4eaa3d281d46
Doug Lima <ytdouglima@gmail.com>
2025-07-03 01:04:47 -0300

    Continuação exercício controle interface e abstrato

---
+++ src/controle/Main.java
@@ -0,0 +1,11 @@
+package controle;
+
+public class Main {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
