41cfa65d23511a976a53ec9e8ed40c00ad825c67
dougs-lima <ytdouglima@gmail.com>
2025-07-08 09:22:06 -0300

    Continuação exercício emoji

--- src/emoji/Lutador.java
+++ src/emoji/Lutador.java
@@ -16,7 +16,7 @@ public class Lutador {
         this.nacionalidade = na;
         this.idade = id;
         this.altura = al;
-        this.peso = pe;
+        setPeso(pe);
         this.vitorias = vi;
         this.derrotas = de;
         this.empates = em;
@@ -24,34 +24,33 @@ public class Lutador {
 
     // Métodos Personalizados
     public void apresentar(){
-        System.out.println("Lutador: " + getNome());
-        System.out.println("Origem: " + getNacionalidade());
-        System.out.println(getIdade() + " anos");
-        System.out.println(getAltura() + "m de altura");
-        System.out.println("Pesando " + getPeso() + "kg");
-        System.out.println("Ganhou: " + getVitorias());
-        System.out.println("Perdeu: " + getDerrotas());
-        System.out.println("Empatou: " + getEmpates());
+        System.out.println("_________________________________________________________");
+        System.out.println("CHEGOU A HORA! Apresentamos o lutador " + this.getNome());
+        System.out.println("Diretamente de " + this.getNacionalidade());
+        System.out.println("com " + this.getIdade() + " anos, e " + this.getAltura() + "m de altura");
+        System.out.println("pesando " + this.getPeso() + "kg");
+        System.out.println(this.getVitorias() + " vitórias");
+        System.out.println(this.getDerrotas() + " derrotas");
+        System.out.println(this.getEmpates() + " empates");
     }
 
     public void status() {
-        System.out.println(getNome());
-        System.out.println("é um peso " + getCategoria());
-        System.out.println(getVitorias() + " vitorias");
-        System.out.println(getDerrotas() + " derrotas");
-        System.out.println(getEmpates() + " empates");
+        System.out.println(getNome() + " é um peso " + this.categoria);
+        System.out.println("Ganhou " + this.getVitorias() + " vezes");
+        System.out.println("Perdeu " + this.getDerrotas() + " vezes");
+        System.out.println("Empatou " + this.getEmpates() + " vezes");
     }
 
     public void ganharLuta() {
-        setVitorias(getVitorias() + 1);
+        setVitorias(this.getVitorias() + 1);
     }
 
     public void perderLuta() {
-        setDerrotas(getDerrotas() + 1);
+        setDerrotas(this.getDerrotas() + 1);
     }
 
     public void empatarLuta() {
-        setEmpates(getEmpates() + 1);
+        setEmpates(this.getEmpates() + 1);
     }
 
     // Métodos Especiais
@@ -93,28 +92,25 @@ public class Lutador {
 
     public void setPeso(float peso) {
         this.peso = peso;
+        this.setCategoria();
     }
 
     public String getCategoria() {
         return categoria;
     }
 
-    public void setCategoria(String categoria) {
+    private void setCategoria() {
         if (this.peso < 52.2) {
             this.categoria = "Inválido";
-        } else
-        if (peso <= 70.3) {
+        } else if (peso <= 70.3) {
             this.categoria = "Leve";
-        } else
-        if (this.peso <= 83.9) {
+        } else if (this.peso <= 83.9) {
             this.categoria = "Médio";
-        } else
-        if (this.peso <= 120.2) {
+        } else if (this.peso <= 120.2) {
             this.categoria = "Pesado";
         } else {
             this.categoria = "Inválido";
         }
-        this.categoria = categoria;
     }
 
     public int getVitorias() {
